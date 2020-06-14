238b2491691fb5bc379cbd2192e30bb746e6679a
Abdallah Hemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-14 08:34:56 +0200

    Create 21. reverse string in js.js

---
+++ JS/21. reverse string in js.js
@@ -0,0 +1,5 @@
+// Reverse string in js
+
+let original = "My name is Abdallah Ahmed Hemdan";
+let reversed = original.split("").reverse().join("");
+console.log(reversed); // 'nadmeH demhA halladbA si eman yM'
